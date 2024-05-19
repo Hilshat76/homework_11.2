2b8d444095489a86bb53b90a2865f06f2aac5c0f
Hilshat76 <hilshat76@gmail.com>
2024-05-19 15:18:38 +0300

    Изменил str_func.py
    Добавил docstring функции str_upper

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 
 def str_upper(text_original):
+    """
+    Преобразует строку в верхний регистр
+    :param text_original: Исходный текст
+    :return: Текст, где все буквы в верхнем регистре
+    """
     return text_original.upper()
